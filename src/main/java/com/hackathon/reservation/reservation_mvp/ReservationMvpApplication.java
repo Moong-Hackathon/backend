278cc7d520ed36646a74525c6015010460087899
JiWon Kim <andrewkimswe@gmail.com>
2025-04-04 14:53:14 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/hackathon/reservation/reservation_mvp/ReservationMvpApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.reservation.reservation_mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackathon/reservation/reservation_mvp/ReservationMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackathon.reservation.reservation_mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
