56f765081ce6e32def7154b4e16d683bf529318d
yeoEun <kye020923@g.hongik.ac.kr>
2025-05-03 04:46:58 +0900

    feature/#45 : 사용자 예약 상태 변경 복구

--- src/main/java/com/hackathon/reservation/reservation_mvp/controller/StoreOwnerController.java
+++ src/main/java/com/hackathon/reservation/reservation_mvp/controller/StoreOwnerController.java
@@ -1,12 +1,9 @@
 package com.hackathon.reservation.reservation_mvp.controller;
 
-import com.hackathon.reservation.reservation_mvp.apipayload.ApiResponse;
-import com.hackathon.reservation.reservation_mvp.converter.ReservationConverter;
 import com.hackathon.reservation.reservation_mvp.converter.StoreConverter;
 import com.hackathon.reservation.reservation_mvp.dto.StoreRequestDto;
 import com.hackathon.reservation.reservation_mvp.dto.StoreResponseDto;
 import com.hackathon.reservation.reservation_mvp.entity.Store;
-import com.hackathon.reservation.reservation_mvp.service.ReservationService;
 import com.hackathon.reservation.reservation_mvp.service.StoreService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -20,11 +17,11 @@ public class StoreOwnerController {
 
     @PatchMapping("/status")
     @Operation(summary = "사장님 예약 받기 상태 변경 API", description = "사장님이 매장의 예약 오픈/닫힘 상태를 변경합니다.")
-    public ApiResponse<StoreResponseDto.StoreStateDto> patchStoreStatus(
+    public StoreResponseDto.StoreStateDto patchStoreStatus(
             @PathVariable("storeId") Long storeId,
             @RequestBody StoreRequestDto.StoreStateDto request
     ) {
         Store store = storeService.patchStoreStatus(storeId, request.getIsReservationOpen());
-        return ApiResponse.onSuccess(StoreConverter.storeStateDto(store));
+        return StoreConverter.storeStateDto(store);
     }
 }
